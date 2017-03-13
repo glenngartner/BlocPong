19fc0707059baa81e53e52c799facc9623744f16
Glenn Gartner <glenngartner@Glenns-MacBook-Pro.local>
2017-03-13 01:45:30 -0400

    fix tsconfig, switch to atom compiler. floating cube works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 define("Background", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    /**
-     * Created by glenn on 2/28/2017.
-     */
     var Background = (function () {
         function Background(canvas, context, color) {
             if (color === void 0) { color = "#D0BAAA"; }
@@ -13,7 +10,6 @@ define("Background", ["require", "exports"], function (require, exports) {
             this.drawBackground(canvas, context, color);
         }
         Background.prototype.drawBackground = function (canvas, context, color) {
-            // draw background fill color
             context.fillStyle = color;
             context.fillRect(0, 0, canvas.width, canvas.height);
         };
@@ -21,9 +17,6 @@ define("Background", ["require", "exports"], function (require, exports) {
     }());
     exports.Background = Background;
 });
-/**
- * Created by glenn on 2/28/2017.
- */
 define("Ball", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -63,9 +56,38 @@ define("Ball", ["require", "exports"], function (require, exports) {
     }());
     exports.Ball = Ball;
 });
-/**
- * Created by glenn on 2/28/2017.
- */
+define("Pong", ["require", "exports", "three"], function (require, exports, THREE) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Pong = (function () {
+        function Pong() {
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            var renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+            var geometry = new THREE.BoxGeometry(1, 1, 1);
+            var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            var cube = new THREE.Mesh(geometry, material);
+            scene.add(cube);
+            camera.position.z = 5;
+            var render = function () {
+                requestAnimationFrame(render);
+                cube.rotation.x += 0.1;
+                cube.rotation.y += 0.1;
+                renderer.render(scene, camera);
+            };
+            render();
+        }
+        return Pong;
+    }());
+    exports.Pong = Pong;
+});
+define("main", ["require", "exports", "Pong"], function (require, exports, Pong_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var pong = new Pong_1.Pong;
+});
 define("Paddle", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -101,65 +123,5 @@ define("Paddle", ["require", "exports"], function (require, exports) {
         return Paddle;
     }());
     exports.Paddle = Paddle;
-});
-/**
- * Created by glenn on 2/28/2017.
- */
-define("Pong", ["require", "exports", "Background", "Paddle", "Ball"], function (require, exports, Background_1, Paddle_1, Ball_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var Pong = (function () {
-        function Pong(canvas, context) {
-            this.canvas = canvas;
-            this.context = context;
-            var background = new Background_1.Background(canvas, context);
-            this.createPlayers(this.context);
-            this.createBall(context);
-            this.render();
-        }
-        Pong.prototype.createPlayers = function (context) {
-            this.computer = new Paddle_1.Paddle(context, 25, 25);
-            this.player = new Paddle_1.Paddle(context, this.canvas.width - 75, this.canvas.height - 200);
-        };
-        Pong.prototype.createBall = function (context) {
-            this.ball = new Ball_1.Ball(context, 350, 200);
-        };
-        Pong.prototype.render = function () {
-            this.computer.render();
-            this.player.render();
-        };
-        return Pong;
-    }());
-    exports.Pong = Pong;
-});
-define("main", ["require", "exports", "three"], function (require, exports, THREE) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    // let canvas = <HTMLCanvasElement>document.getElementById('pongCanvas');
-    // configureCanvas(canvas);
-    // let context = canvas.getContext('2d');
-    // let pong = new Pong(canvas, context);
-    //
-    // function configureCanvas(canvas){
-    //     canvas.width = 1200;
-    //     canvas.height = 800;
-    // }
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var render = function () {
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
-        renderer.render(scene, camera);
-    };
-    render();
 });
 //# sourceMappingURL=main.js.map
--- ts/Pong.ts
+++ ts/Pong.ts
@@ -1,34 +1,61 @@
 /**
  * Created by glenn on 2/28/2017.
  */
-
-import {Background} from "Background";
-import {Paddle} from "Paddle";
-import {Ball} from "Ball";
+import * as THREE from "three";
+// import {Background} from "Background";
+// import {Paddle} from "Paddle";
+// import {Ball} from "Ball";
 
 export class Pong {
-    computer: Paddle;
-    player: Paddle;
-    ball: Ball;
+    // computer: Paddle;
+    // player: Paddle;
+    // ball: Ball;
 
-    constructor(public canvas:HTMLCanvasElement, public context: CanvasRenderingContext2D){
-        let background = new Background(canvas, context);
-        this.createPlayers(this.context);
-        this.createBall(context);
-        this.render();
-    }
+    // constructor(public canvas:HTMLCanvasElement, public context: CanvasRenderingContext2D){
+    //     let background = new Background(canvas, context);
+    //     this.createPlayers(this.context);
+    //     this.createBall(context);
+    //     this.render();
+    // }
 
-    createPlayers(context){
-        this.computer = new Paddle(context, 25, 25);
-        this.player = new Paddle(context, this.canvas.width - 75, this.canvas.height-200);
-    }
+    // createPlayers(context){
+    //     this.computer = new Paddle(context, 25, 25);
+    //     this.player = new Paddle(context, this.canvas.width - 75, this.canvas.height-200);
+    // } 
 
-    createBall(context){
-        this.ball = new Ball(context, 350, 200);
-    }
+    // createBall(context){
+    //     this.ball = new Ball(context, 350, 200);
+    // }
 
-    render(){
-        this.computer.render();
-        this.player.render();
+    // render(){
+    //     this.computer.render();
+    //     this.player.render();
+    // }
+
+    constructor() {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        var render = function () {
+            requestAnimationFrame(render);
+
+            cube.rotation.x += 0.1;
+            cube.rotation.y += 0.1;
+
+            renderer.render(scene, camera);
+        };
+
+        render();
     }
 }
