41a0c7adf4bd7b5c76300a943ec2123c6a4735eb
Glenn Gartner <glenngartner@gmail.com>
2017-04-12 03:20:50 -0400

    remove build scripts from git tracker

--- ts/core/renderer_config.ts
+++ ts/core/renderer_config.ts
@@ -1,7 +1,7 @@
 
 export let graphicsLibs: rendererConfig = {
-  three: true,
-  babylon: false
-  // three: false,
-  // babylon: true
+  // three: true,
+  // babylon: false
+  three: false,
+  babylon: true
 }
