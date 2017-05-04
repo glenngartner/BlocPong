dda168f1f12a0f2b57aea575b5aedd5241fdc73e
Glenn Gartner <glenngartner@gmail.com>
2017-05-04 15:38:53 -0400

    refactor generic render class, add do function to loop over frameworks

--- ts/core/GenericRenderer.ts
+++ ts/core/GenericRenderer.ts
@@ -22,47 +22,74 @@ export class GenericRenderer implements Renderer {
             if (renderer == "babylonjs") {
                 this.frameworks.push(new BabylonRenderer());
                 console.log("using babylonJS");
-            } if (renderer == "threejs") {
+            }
+
+            if (renderer == "threejs") {
                 this.frameworks.push(new ThreeRenderer());
                 console.log("using threeJS");
             }
         }
     }
 
-    do(array: Array<Renderer>, func: Function) {
-        for (let renderer of array) {
-            // renderer.func;
+    private do(func: string, ...args ) {
+        for (let renderer of this.frameworks) {
+            switch (func) {
+                case "createScene": renderer.createScene();
+                break;
+
+                case "createCamera": renderer.createCamera();
+                break;
+
+                case "createBackground": renderer.createBackground();
+                break;
+
+                case "createActor": renderer.createActor(args);
+                // case "createActor": renderer.createActor.apply(renderer, args);
+                break;
+
+                case "createMaterial": renderer.createMaterial();
+                break;
+
+                case "createDirectionalLight": renderer.createDirectionalLight();
+                break;
+
+                case "render": renderer.render();
+                break;
+            }
         }
     }
 
     createScene() {
-        for (let framework of this.frameworks){framework.createScene();
-        }
+        this.do("createScene");
+        // for (let framework of this.frameworks){framework.createScene()};
     };
 
     createCamera() {
-        for (let framework of this.frameworks){framework.createCamera();
-        }
+        // for (let framework of this.frameworks){framework.createCamera()};
+        this.do("createCamera");
     }
 
     createBackground() {
-        for (let framework of this.frameworks){framework.createBackground()};
+        // for (let framework of this.frameworks){framework.createBackground()};
+        this.do("createBackground");
     }
 
     createActor(actor: Actor) {
         for (let framework of this.frameworks){framework.createActor(actor)};
+        // this.do("createActor", arguments);
     };
-
     createMaterial() {
-        for (let framework of this.frameworks){framework.createMaterial()};
+        this.do("createMaterial");
     };
 
     createDirectionalLight() {
-        for (let framework of this.frameworks){framework.createDirectionalLight()};
+        this.do("createDirectionalLight");
+        // for (let framework of this.frameworks){framework.createDirectionalLight()};
     };
 
     render() {
-        for (let framework of this.frameworks){framework.render()};
+        this.do("render");
+        // for (let framework of this.frameworks){framework.render()};
     };
 
 }
